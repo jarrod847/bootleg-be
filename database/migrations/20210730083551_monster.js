e7c20a073c5bcaa3f0bc659850520314ff838d75
jarrodskahill <jarrodskahill@gmail.com>
2021-07-30 03:43:23 -0500

    created a table for monsters

--- database/migrations/20210730083551_monster.js
+++ database/migrations/20210730083551_monster.js
@@ -1,8 +1,17 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("monster", (mon) => {
+    mon.increments("").unique();
+    mon.string("name").NotNullable().unique();
+    mon.integer("Lvl");
+    mon.integer("HP");
+    mon.integer("Str");
+    mon.integer("Dex");
+    mon.integer("MP");
+    mon.integer("Location");
+    mon.integer("yExp");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("monster");
 };
