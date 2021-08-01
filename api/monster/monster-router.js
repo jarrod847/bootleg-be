ac75923842d6468fdf40083970a1f99bf34ffd92
jarrodskahill <jarrodskahill@gmail.com>
2021-07-31 23:22:42 -0500

    created routes to add and delete monsters

--- api/monster/monster-router.js
+++ api/monster/monster-router.js
@@ -20,3 +20,28 @@ router.get("/:id", async (req, res) => {
     res.status(500).json({ message: "could not get monster", error: e });
   }
 });
+
+router.post("/add", async (req, res) => {
+  try {
+    const newMonster = await Monster.addMonster(req.body);
+    res
+      .status(200)
+      .json({ message: "you have created a monster", monster: newMonster });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: "could not create a new monster", error: e });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedMon = await Monster.deleteMonster(req.params.id);
+    res
+      .status(200)
+      .json({ message: "monster was deleted", monster: deletedMon });
+  } catch (e) {
+    res.status.json({ message: "could not delete monster", error: e });
+  }
+});
+module.exports = router;
