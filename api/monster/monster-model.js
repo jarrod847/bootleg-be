4013b4487ed1f85e1fa709cd279d91297a86c30b
jarrodskahill <jarrodskahill@gmail.com>
2021-07-31 23:08:18 -0500

    created more helper functions for monsters; add, remove, and update

--- api/monster/monster-model.js
+++ api/monster/monster-model.js
@@ -15,3 +15,19 @@ function getAllMonsters() {
 function getMonsterById(id) {
   return db("monster").where("id", id).first();
 }
+
+function addMonster(monster) {
+  return db("monster")
+    .insert(monster, "id")
+    .then(([id]) => {
+      return getMonsterById(id);
+    });
+}
+
+function updateMonster(id, changes) {
+  return getMonsterById(id).update({ changes }).returning("*");
+}
+
+function deleteMonster(id) {
+  return getMonsterById(id).del();
+}
