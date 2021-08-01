f2a92c761bffb273f9a27dc1aa1aeb115ca99a78
jarrodskahill <jarrodskahill@gmail.com>
2021-07-31 22:53:15 -0500

    removed auth0 stuff for now, updated monster table

--- server.js
+++ server.js
@@ -2,23 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const server = express();
-const { auth } = require("express-openid-connect");
+// const { auth } = require("express-openid-connect");
 const playerRouter = require("./player/player-router");
 require("dotenv").config();
-server.use(
-  auth({
-    authRequired: false,
-    auth0Logout: true,
-    issuerBaseURL: process.env.ISSUER_BASE_URL,
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.CLIENT_ID,
-    secret: process.env.SECRET,
-  })
-);
+// server.use(
+//   auth({
+//     authRequired: false,
+//     auth0Logout: true,
+//     issuerBaseURL: process.env.ISSUER_BASE_URL,
+//     baseURL: process.env.BASE_URL,
+//     clientID: process.env.CLIENT_ID,
+//     secret: process.env.SECRET,
+//   })
+// );
 
-server.get("/", (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
-});
+// server.get("/", (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+// });
 
 server.use(helmet());
 server.use(cors());
