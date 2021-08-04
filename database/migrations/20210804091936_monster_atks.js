8aae7d56093fd2f5d129832b8527fdd40b4e20ec
jarrodskahill <jarrodskahill@gmail.com>
2021-08-04 04:26:30 -0500

    created a monster attacks table and updated attaks tbl

---
+++ database/migrations/20210804091936_monster_atks.js
@@ -0,0 +1,37 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("mon_atk", (wpn) => {
+    wpn.increments("id").unique();
+    wpn
+      .integer("attack_1")
+      .unsigned()
+      .references("id")
+      .inTable("attack")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    wpn
+      .integer("attack_2")
+      .unsigned()
+      .references("id")
+      .inTable("attack")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    wpn
+      .integer("attack_3")
+      .unsigned()
+      .references("id")
+      .inTable("attack")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    wpn
+      .integer("attack_4")
+      .unsigned()
+      .references("id")
+      .inTable("attack")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("mon_atk");
+};
