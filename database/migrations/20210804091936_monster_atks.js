5d74662ac9fdd51179f233f7af6526da2a342c2b
jarrodskahill <jarrodskahill@gmail.com>
2021-08-04 04:32:34 -0500

    changes to monster atk tbl order

--- database/migrations/20210804091936_monster_atks.js
+++ database/migrations/20210804091936_monster_atks.js
@@ -2,7 +2,15 @@ exports.up = function (knex) {
   return knex.schema.createTable("mon_atk", (wpn) => {
     wpn.increments("id").unique();
     wpn
-      .integer("attack_1")
+      .integer("mon_Id")
+      .unique()
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("monster")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE")
+      .wpn.integer("attack_1")
       .unsigned()
       .references("id")
       .inTable("attack")
