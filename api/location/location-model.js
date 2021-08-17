15edaa1efda98879c815840b0a2f358a7ab3d453
jarrodskahill <jarrodskahill@gmail.com>
2021-08-16 23:44:17 -0500

    createad some more helper functions for locations

--- api/location/location-model.js
+++ api/location/location-model.js
@@ -7,3 +7,19 @@ module.exports = {
   getLocationById,
   getAllLocations,
 };
+
+function getAllLocations() {
+  return db("location");
+}
+
+function getLocationById(id) {
+  return db("location").where("id", id).first();
+}
+
+function addLocation(location) {
+  return db("location")
+    .insert(location, "id")
+    .then(([id]) => {
+      return getLocationById(id);
+    });
+}
