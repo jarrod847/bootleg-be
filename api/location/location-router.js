ea6ca8e732ea79b9d6eb14e2f1864c9434232603
jarrodskahill <jarrodskahill@gmail.com>
2021-08-19 22:42:19 -0500

    new route for adding locations

--- api/location/location-router.js
+++ api/location/location-router.js
@@ -18,3 +18,17 @@ router.get("/:id", async (req, res) => {
     res.status(500).json({ message: "could not get location", error: e });
   }
 });
+
+router.post("/add", async (req, res) => {
+  try {
+    const newLocation = await Location.addLocation(req.body);
+    res
+      .status(200)
+      .json({ message: "you have created a location", loc: newLocation });
+  } catch (e) {
+    console.log(e);
+    res
+      .status(500)
+      .json({ message: "could not create a new location", error: e });
+  }
+});
