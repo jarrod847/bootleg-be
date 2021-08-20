0a2cd81248c477c869f0e5c7efe2c2f4a649f080
jarrodskahill <jarrodskahill@gmail.com>
2021-08-19 23:22:27 -0500

    updated player gear table

--- database/migrations/20210820040303_player_gear.js
+++ database/migrations/20210820040303_player_gear.js
@@ -1,8 +1,60 @@
+const { WSAEOPNOTSUPP } = require("node:constants");
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("player_gear", (tbl) => {
+    tbl.increments("id").unique();
+    tbl
+      .integer("head")
+      .unsigned()
+      .references("id")
+      .inTable("gear")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    tbl
+      .integer("body")
+      .unsigned()
+      .references("id")
+      .inTable("gear")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    tbl
+      .integer("gloves")
+      .unsigned()
+      .references("id")
+      .inTable("gear")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    tbl
+      .integer("l_hand")
+      .unsigned()
+      .references("id")
+      .inTable("weapon")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    tbl
+      .integer("r_hand")
+      .unsigned()
+      .references("id")
+      .inTable("weapon")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    tbl
+      .integer("legs")
+      .unsigned()
+      .references("id")
+      .inTable("gear")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    tbl
+      .integer("feet")
+      .unsigned()
+      .references("id")
+      .inTable("gear")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("player_gear");
 };
