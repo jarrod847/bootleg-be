563e96bfd81ddf0cb6a36803a9d6834cf0b6a5aa
jarrodskahill <jarrodskahill@gmail.com>
2021-08-26 05:07:08 -0500

    created some helper function for player stats

---
+++ api/player/playerStats-model.js
@@ -0,0 +1,24 @@
+const db = require("../../database/config");
+
+module.exports = {
+  playerStats,
+  updateStats,
+  addStats,
+  allStats,
+};
+
+function playerStats(id) {
+  return db("player_stats").where("player_id", id).first();
+}
+
+function addStats(id) {
+  return db("player_stats")
+    .insert({ player_id: id })
+    .then(() => {
+      return playerStats(id);
+    });
+}
+
+function allStats() {
+  return db("player_stats");
+}
