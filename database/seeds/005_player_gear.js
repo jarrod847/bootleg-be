58f6b9591e6a691257a38c9668e2d82ce4470e47
jarrodskahill <jarrodskahill@gmail.com>
2022-05-08 01:07:49 -0700

    added gear creation on account creation

--- database/seeds/005_player_gear.js
+++
@@ -1,9 +0,0 @@
-exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex("player_gear")
-    .del()
-    .then(function () {
-      // Inserts seed entries
-      return knex("player_gear").insert([{ id: 1, player_id: 1, r_hand: 1 }]);
-    });
-};
