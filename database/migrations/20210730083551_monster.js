ab6fb98841f3908ecd375c019daa08f334df1a3e
jarrodskahill <jarrodskahill@gmail.com>
2022-05-08 21:41:46 -0700

    created max mp for both monster and user, also added total hp to monster

--- database/migrations/20210730083551_monster.js
+++ database/migrations/20210730083551_monster.js
@@ -2,11 +2,13 @@ exports.up = function (knex) {
   return knex.schema.createTable("monster", (mon) => {
     mon.increments("id").unique();
     mon.string("name").unique();
+    mon.integer("total_hp");
     mon.integer("Lvl");
     mon.integer("Hp");
     mon.integer("Str");
     mon.integer("Def");
     mon.integer("Mp");
+    mon.integer("max_mp");
     mon.integer("Shield").default(0);
     mon.integer("Loc");
     mon.integer("yExp");
