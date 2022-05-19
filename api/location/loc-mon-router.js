1eb70250e4a5f3b51e84aae4eeec56eb1ece7aff
jarrodskahill <jarrodskahill@gmail.com>
2022-05-18 22:07:47 -0700

    created a route to get all monsters found at locations

---
+++ api/location/loc-mon-router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const LocMons = require("./loc-mon-model");
+
+router.get("/", (req, res) => {
+  LocMons.allLocMons()
+    .then((locMons) => {
+      res.status(201).json(locMons);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "could not get monsters for locations",
+      });
+    });
+});
