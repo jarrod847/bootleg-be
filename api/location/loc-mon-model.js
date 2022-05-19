1b097a9a030216b3b11eaada75a47d72dde0e18a
jarrodskahill <jarrodskahill@gmail.com>
2022-05-18 22:03:27 -0700

    created helper function to add and remove location monsters

--- api/location/loc-mon-model.js
+++ api/location/loc-mon-model.js
@@ -3,6 +3,7 @@ const db = require("../database/config");
 module.exports = {
   allLocMons,
   findLocMonsById,
+  addLocMons,
 };
 
 function allLocMons() {
@@ -11,4 +12,16 @@ function allLocMons() {
 
 function findLocMonsById(id) {
   return db("location_monsters").where("id", id).first();
+}
+
+function addLocMons() {
+  return db("location_monsters")
+    .insert(monsters, "id")
+    .then(([id]) => {
+      return findLocMonsById(id);
+    });
+}
+
+function delLocMons(id) {
+  return findLocMonsById(id).del();
 }
