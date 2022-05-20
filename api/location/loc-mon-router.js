d776df8c5566c3f041f2dfa7a095eda0ad26146a
jarrodskahill <jarrodskahill@gmail.com>
2022-05-19 20:14:12 -0700

    created a new route to get monsters for a location by id

--- api/location/loc-mon-router.js
+++ api/location/loc-mon-router.js
@@ -30,4 +30,18 @@ router.post("/add", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  LocMons.findLocMonsById(req.params.id)
+    .then((locMon) => {
+      res
+        .status(200)
+        .json({ message: "successfully got monsters for location by id" });
+    })
+    .catch((e) => {
+      res
+        .status(500)
+        .json({ message: "could not get monsters for location", error: e });
+    });
+});
+
 module.exports = router;
